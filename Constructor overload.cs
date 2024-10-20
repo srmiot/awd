b43b3d64b6b61aa78f3ce8ebfc71f79aaffa7447
srmiot <uzzodot@gmail.com>
2024-10-20 21:31:37 +0530

    Constructor overload.cs

---
+++ Constructor overload.cs
@@ -0,0 +1,59 @@
+using System:
+
+namespace prat
+
+class Test
+
+int x, y String Name: public Test()
+
+x-y-25; Name = "chitra":
+
+1 public Test(int x, int y,String Name)
+
+this.x-x;
+
+this.yy: this. Name Name:
+
+public Test(String Name,int x)
+
+this.Name Name;
+
+this.x this y= x;
+
+public void show()
+
+{ Console.WriteLine("the mark of subl " + x); Console.WriteLine("the marks of sub2" + y); Console.WriteLine("name of the student"+ Name);
+
+}
+
+}
+
+internal class Program
+
+{
+
+static void Main(string[] args)
+
+{
+
+Test test new Test(34,21,"chitra");
+
+test.show();
+
+Console.WriteLine();
+
+Test test1 new Test();
+
+test1.show();
+
+Console.WriteLine();
+
+Test test2 = new Test("Divya", 18);
+
+test2.show();
+
+Console.ReadKey();
+
+}
+
+}
