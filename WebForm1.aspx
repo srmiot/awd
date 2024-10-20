fef98d943bfdf564ef599f352243988fb8815fee
srmiot <uzzodot@gmail.com>
2024-10-20 23:15:03 +0530

    Connected

---
+++ WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="temp2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Enter Details:<br />
+            Roll No :
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            Name :&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            Address :
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Delete" />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace temp2
+{
+    //connected data architecture
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\TANMAY\source\repos\temp2\App_Data\Database1.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd1 = new SqlCommand("select * from stud", con);
+            con.Open();
+            SqlDataReader dr = cmd1.ExecuteReader();
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into stud values("+ Convert.ToInt32(TextBox1.Text) +",'"+ TextBox2.Text +"','"+ TextBox3.Text +"')",con);
+            SqlCommand cmd1 = new SqlCommand("select * from stud", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Label1.Text = "Inserted succesfully!!!";
+            SqlDataReader dr = cmd1.ExecuteReader();
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("delete from stud where roll=("+ Convert.ToInt32(TextBox1.Text)+")", con);
+            SqlCommand cmd1 = new SqlCommand("select * from stud", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Label1.Text = "Deleted succesfully!!!";
+            SqlDataReader dr = cmd1.ExecuteReader();
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+            con.Close();
+        }
+    }
+}
