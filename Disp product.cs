c3b2ec236a9402f5c6499611a6073d936abd8669
srmiot <uzzodot@gmail.com>
2024-10-20 21:47:45 +0530

    Disp product.cs

---
+++ Disp product.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Pract_1
+
+{
+
+internal class Program
+
+{
+
+static void Main(string[] args)
+
+{
+
+Console.WriteLine("Ricky 13");
+
+Console.WriteLine("Enter num_1 : "); int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter num_2 : "); int n2 = Convert.ToInt32(Console.ReadLine()); Console.WriteLine("Enter num_3 : ");
+
+int n3 = Convert.ToInt32(Console.ReadLine()); Console.WriteLine("Enter num_4 : "); int n4 = Convert.ToInt32(Console.ReadLine()); Console.WriteLine("Product : " + (n1*n2*n3*n4)); Console.ReadKey();
+
+}
+
+}
+
+}
