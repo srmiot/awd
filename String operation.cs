9138d1ec7e4fed8e3ca470bbbd0e1cb2ce492801
srmiot <uzzodot@gmail.com>
2024-10-20 21:45:39 +0530

    String operation.cs

---
+++ String operation.cs
@@ -0,0 +1,45 @@
+using System;
+
+using System.Linq;
+
+namespace Pract1._2
+
+{
+
+internal class Program
+
+{
+
+static void Main(string[] args)
+
+{
+
+Console.WriteLine("Ricky 13");
+
+String s1 = "RICKY"; String s2 = "fernandis";
+
+String s3 = "KASAL";
+
+Console.WriteLine("Length of String fernandis: " + s2.Length);
+
+Console.WriteLine("Position of letter C in RICKY : " + s1.IndexOf("C"));
+
+Console.WriteLine("Original string : " + s3);
+
+Console.WriteLine("String in upper case: " + s2.ToUpper());
+
+Console.WriteLine("String" + s1 + " and " + s2 + " are equal: " + sl.Equals(s2)
+
+Console.WriteLine("String" + s3 + " starts with K: " + s3.StartsWith("K"));
+
+Console.WriteLine("String" + s1 + " ends with R: " + s3.EndsWith("R"));
+
+Console.WriteLine("Extract the portion of " + s3 + " is: " + s3.Substring(2));
+
+Console.ReadKey();
+
+}
+
+}
+
+                  }
