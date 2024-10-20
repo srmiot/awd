6a475e9ca401e9dffbc7b03b1aeab8563fbea05e
srmiot <uzzodot@gmail.com>
2024-10-20 21:42:38 +0530

    Prime.cs

---
+++ Prime.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace PrimeNum
+
+{
+
+public class Prime
+
+{
+
+static void Main(string[] args) {
+
+int flag = 0 , s = 2 num;
+
+Console.WriteLine("Ricky 13");
+
+Console.Write("Enter the number: ");
+
+num = Convert.ToInt32(Console.ReadLine()); Console.Write("\n"); while (s < num / 2)
+
+{
+
+if ( num \%s=0)
+
+{
+
+flag = 1;
+
+break;
+
+}
+
+s++;
+
+}
+
+if (flag= 0 )
+
+{
+
+}
+
+Console.WriteLine ( num + n is Prime number");
+
+else
+
+{
+
+}
+
+Console.WriteLine(num + " is not A Prime number");
+
+}}}
